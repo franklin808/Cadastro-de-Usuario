b04abe897bc88a06db3f60f1a7a33d3e92bae72a
franklin808 <franklingaio@Yahoo.com.br>
2025-05-26 11:31:56 -0300

    Remoção do Supabase e utilização de arquivo txt

--- app.py
+++ app.py
@@ -12,9 +12,32 @@ def cadastrar():
     email = request.form['email']
     senha = request.form['senha']
 
-    print(f'Cadastro: {nome}, {email}, {senha}')
-    
+    with open('usuarios.txt', 'a', encoding='utf-8') as arquivo:
+        arquivo.write(f'Nome: {nome} | Email: {email} | Senha: {senha}\n')
+        return render_template('cadastrar.html', nome=nome)        
+        
+
     return f'Usuário {nome} cadastrado com sucesso!'
+
+@app.route('/login', methods=['POST'])
+def login():
+    email_digitado = request.form['email']
+    senha_digitada = request.form['senha']
+
+    try:
+        with open('usuarios.txt', 'r', encoding='utf-8') as arquivo:
+            usuarios = arquivo.readlines()
+    except FileNotFoundError:
+        return "Nenhum usuário cadastrado ainda."
+
+    for linha in usuarios:
+        if f'Email: {email_digitado} | Senha: {senha_digitada}' in linha:
+            nome_inicio = linha.find('Nome: ') + 6
+            nome_fim = linha.find(' | Email')
+            nome_usuario = linha[nome_inicio:nome_fim]
+            return render_template('login.html', nome_usuario=nome_usuario)
+    
+    return 'E-mail ou senha incorretos.'
 
 if __name__ == '__main__':
     app.run(debug=True)
